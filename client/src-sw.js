c7123888920daac0916bd6ecd977132557684ba6
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2022-01-22 15:53:20 -0700

    removed comments from src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-// const { StaleWhileRevalidate } = require("workbox-strategies");
+const { StaleWhileRevalidate } = require("workbox-strategies");
 const { CacheFirst } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
@@ -27,7 +27,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
+//Implements asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
