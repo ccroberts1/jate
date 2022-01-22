c4fdf5028d3905b1adddb1816fcc3e6fc9610e4c
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2022-01-22 12:23:03 -0700

    started on install logic for pwa and added image cache routing to src-sw

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,33 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+//TODO: Figure out variables for cacheName and cachedResources based on src-sw.js configs
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  console.log("Install event");
+  deferredPrompt = event;
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(cachedResources);
+    })
+  );
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  console.log("Activate event");
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("Fetch intercepted for:", event.request.url);
+});
+
+//Code for offline loading from GCD PW training
+// window.addEventListener("fetch", event => {
+//     event.respondWith(caches.match(event.request)
+// .then(cachedResponse => {
+//     return cachedResponse || fetch(event.request)
+// }));
+// })
