3e6f4d3d981a956d7bb7bc4cf04a5437851705f1
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2022-01-22 15:49:34 -0700

    removed comments from install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,6 @@
 const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-//TODO: Figure out variables for cacheName and cachedResources based on src-sw.js configs
+// Logic for installing PWA
 window.addEventListener("beforeinstallprompt", (event) => {
   console.log("Install event");
   //Stores the event
@@ -11,7 +9,6 @@ window.addEventListener("beforeinstallprompt", (event) => {
   butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
   console.log("Activate event");
   const promptEvent = window.deferredPrompt;
@@ -29,9 +26,7 @@ butInstall.addEventListener("click", async () => {
   butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  console.log("Fetch intercepted for:", event.request.url);
   //Clears install prompt
   window.deferredPrompt = null;
 });
