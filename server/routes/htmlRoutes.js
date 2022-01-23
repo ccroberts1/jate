57fee7583b1c04b63034cc8db4c83e9bdaf1b917
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2022-01-22 17:36:06 -0700

    fixed issue with sw by removing extra empty registeroute

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,10 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+module.exports = (app) => {
+  app.get("/service-worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
+  });
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
   );
+};
